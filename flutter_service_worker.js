fe35beec8d29116753e22fcebc1bcd5d4c93fab9
Aung Myo Min <70466675+aungmyominoffical@users.noreply.github.com>
2024-12-16 18:29:10 +0700

    Update Servers Lists

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "edbb7768e3ba44efa7e182271651c2b1",
+const RESOURCES = {"flutter_bootstrap.js": "07978b3437d5b871ab94592e2ee114ca",
 "version.json": "a556f5e2ebd43e22108d5642d8fe6bda",
 "index.html": "5e9f2715c6684ca8cfcd63b571f43b90",
 "/": "5e9f2715c6684ca8cfcd63b571f43b90",
-"main.dart.js": "37f50cea7cbe2c62a17acf668f5a418b",
+"main.dart.js": "1d3bc1b30240981c4f6a694db6a0797d",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
